9edbd1e9ff749239daefab1b3ede3df4fcd22306
Nick <kolya27.961@gmail.com>
2022-10-10 21:53:31 +0300

    Make download and save func. Also make save_img func more universal

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pty import slave_open
 import requests
 from pathlib import Path
 
@@ -9,18 +10,24 @@ def download_img(link):
     return response.content
 
 
-def save_img(img_folder, img_name, img_binary):
-    Path(img_folder).mkdir(parents=True, exist_ok=True)
-    with open(f'{img_folder}/{img_name}', 'wb') as img_file:
+def save_img(img_path, img_binary):
+    folder = Path(img_path).resolve().parent
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    with open(img_path, 'wb') as img_file:
         img_file.write(img_binary)
 
 
+def download_and_save(link, path):
+    img_binary = download_img(link)
+    save_img(path, img_binary)
+
+
 def main():
-    img_binary = download_img(
+    img_binary = download_and_save(
         'https://upload.wikimedia.org/wikipedia/'
-        'commons/3/3f/HST-SM4.jpeg'
+        'commons/3/3f/HST-SM4.jpeg',
+        'images/hubble.jpeg'
     )
-    save_img('images', 'hubble.jpeg', img_binary)
 
 
 if __name__ == '__main__':
