a45ed40d612017a68c5ca87dbcdbd118830ddb1a
Nick <kolya27.961@gmail.com>
2023-01-12 17:49:57 +0300

    Publish photo to Telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -8,7 +8,10 @@ def main():
     load_dotenv()
     api_key = os.getenv('DVMN_TG_BOT_API_KEY')
     bot = telegram.Bot(token=api_key)
-    bot.send_message(chat_id='@edtestchan', text='Hello, World!')
+    bot.send_photo(
+        chat_id='@edtestchan',
+        photo=open('images/epic_2023-01-11 00:13:03.png', 'rb')
+    )
 
 
 if __name__ == '__main__':
